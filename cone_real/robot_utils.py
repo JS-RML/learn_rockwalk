b2a6734a6293466be0ccd9c7f484a91bd3e3fd28
Abdullah Nazir <sanazir@connect.ust.hk>
2021-09-24 13:32:57 +0800

    cone_real

---
+++ cone_real/robot_utils.py
@@ -0,0 +1,26 @@
+import urx
+import rospy
+import math3d as m3d
+
+
+class RobotSetup:
+
+    def __init__(self, robot_ip):
+
+        self._robot = urx.Robot(robot_ip, use_rt=True)
+        rospy.sleep(1)
+
+    def get_robot(self):
+        return self._robot
+
+    def set_home_configuration(self):
+
+        rospy.loginfo("Moving robot arm to home configuration")
+        pose = m3d.Transform()
+        pose.pos = m3d.Vector([0.15, -0.8, 0.4]) #0.45
+        pose.orient = m3d.Orientation([0, 1, 0,
+                                       1, 0, 0,
+                                       0, 0, -1])
+
+        self._robot.set_pose(pose, acc=0.05, vel=0.05, wait=False)
+        rospy.sleep(0.5)
